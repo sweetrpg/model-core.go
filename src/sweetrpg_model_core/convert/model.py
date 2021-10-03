9d2fcdba8907c4f1cab651054dc95fe037ab3d88
Paul Schifferer <paul@schifferers.net>
2021-10-03 13:55:37 -0700

    Start filling out convert function

--- src/sweetrpg_model_core/convert/model.py
+++ src/sweetrpg_model_core/convert/model.py
@@ -3,6 +3,10 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 """
 
+import logging
 
-def convert_document():
-    pass
+
+def convert_document(doc, model_class):
+    logging.debug("doc: %s, model_class: %s", doc, model_class)
+    model = model_class()
+    logging.debug("model: %s", model)
