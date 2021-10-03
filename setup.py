1324d87a88a2dc8cdd641b50cfd77f4f04205ee4
Paul Schifferer <paul@schifferers.net>
2021-10-03 05:55:32 -0700

    Initialize python

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="PACKAGE_NAME",
+    install_requires=["name==version"],
+    extras_require={},
+)
