a76f7a379b7bda1c889d5ccf8f2822d6945bade7
Paul Schifferer <paul@schifferers.net>
2021-10-04 06:46:27 -0700

    Add embedded schema

--- src/sweetrpg_model_core/schema/base.py
+++ src/sweetrpg_model_core/schema/base.py
@@ -5,7 +5,9 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 
 from marshmallow import Schema, fields, EXCLUDE
 from marshmallow import pre_load
+from marshmallow import post_load
 from datetime import datetime
+import logging
 
 
 class BaseSchema(Schema):
@@ -19,21 +21,34 @@ class BaseSchema(Schema):
 
     @pre_load
     def handle_id(self, in_data, **kwargs):
-        """Converts _id into id
-        """
+        """Converts _id into id"""
         in_data["id"] = in_data.get("_id") or in_data.get("id")
         return in_data
 
     @pre_load
     def handle_dates(self, in_data, **kwargs):
-        """Converts date/time value to an ISO formatted string.
-        """
+        """Converts date/time value to an ISO formatted string."""
         for k in ["created_at", "updated_at", "deleted_at"]:
             if isinstance(in_data.get(k), datetime):
                 in_data[k] = in_data[k].isoformat()
         return in_data
 
+    @post_load
+    def make_object(self, data, **kwargs):
+        logging.info("data: %s", data)
+        return self.model_class(**data)
+
     id = fields.Str()  # as_string=True, dump_only=True)
     created_at = fields.DateTime(required=True)
     updated_at = fields.DateTime(required=True)
     deleted_at = fields.DateTime(allow_none=True)
+
+
+class BaseEmbeddedSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        logging.info("data: %s", data)
+        return self.model_class(**data)
