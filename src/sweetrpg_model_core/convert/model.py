d61ddde0a1269368d16fa848be7791c6437e9737
Paul Schifferer <paul@schifferers.net>
2021-10-04 21:22:46 -0700

    Convert DB document to model instance

--- src/sweetrpg_model_core/convert/model.py
+++ src/sweetrpg_model_core/convert/model.py
@@ -8,5 +8,9 @@ import logging
 
 def convert_document(doc, model_class):
     logging.debug("doc: %s, model_class: %s", doc, model_class)
-    model = model_class()
+    data = doc.to_json()
+    logging.debug("doc: %s, data: %s", doc, data)
+    model = model_class(**data)
     logging.debug("model: %s", model)
+
+    return model
