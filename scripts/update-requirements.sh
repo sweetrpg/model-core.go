5bb32b304d34435189e39a28c69ab78aa3b2a4ef
Paul Schifferer <paul@schifferers.net>
2021-10-30 21:25:15 -0700

    Update requirements

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -6,8 +6,9 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 pushd ${scriptdir}/..
 
-for d in pkg dev docs tests; do
-    pip-compile -r requirements/$d.in
+for r in pkg dev docs tests; do
+    echo "Requirement: $r"
+    pip-compile -r -U requirements/$r.in
 done
 
 popd
