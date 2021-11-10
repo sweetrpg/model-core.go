537f52f2ff3f62b26e6d0a1c4e88f6fc2dfbe525
Paul Schifferer <paul@schifferers.net>
2021-11-10 07:59:24 -0800

    Lang string schema

---
+++ src/sweetrpg_model_core/schema/lang.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Schema for language strings.
+"""
+
+from marshmallow.fields import Mapping
+
+
+class LangStringField(Mapping):
+
+    mapping_type = dict
