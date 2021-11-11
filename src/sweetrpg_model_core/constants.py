8d74ec5c8dd3d524f2df5aae59fabb1c68f61557
Paul Schifferer <paul@schifferers.net>
2021-11-11 14:36:57 -0800

    Doc strings

--- src/sweetrpg_model_core/constants.py
+++ src/sweetrpg_model_core/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
-"""constants.py
+"""Constants.
 """
 
 SYSTEM_USER_ID = "_system"
--- src/sweetrpg_model_core/convert/document.py
+++ src/sweetrpg_model_core/convert/document.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
-"""
+"""Document conversion functions.
 """
 
 import logging
