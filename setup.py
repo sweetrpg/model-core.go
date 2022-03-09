3b806db6a3f1ca363e8ff0e0287fbfcf5f61120c
Paul Schifferer <paul@schifferers.net>
2022-03-08 22:12:51 -0800

    Remove [tls] from setup.py also

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-model-core",
-    install_requires=["marshmallow==3.14.1", "PyMongo[tls,srv]==3.12.1"],
+    install_requires=["marshmallow==3.14.1", "PyMongo[srv]==4.0.2"],
     extras_require={},
 )
