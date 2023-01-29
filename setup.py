53b74f324c6b51d486733d2d15ae3e0d29e684d1
Paul Schifferer <paul@schifferers.net>
2023-01-29 13:21:14 -0800

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-model-core",
-    install_requires=["marshmallow==3.18.0", "PyMongo[srv]==4.0.2"],
+    install_requires=["marshmallow<4.0", "PyMongo[srv]<5.0"],
     extras_require={},
 )
