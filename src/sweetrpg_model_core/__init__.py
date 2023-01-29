260853db93aa60b190d23363126475a860da1b5f
Paul Schifferer <paul@schifferers.net>
2023-01-28 18:47:02 -0800

    Copyright

--- src/sweetrpg_model_core/__init__.py
+++ src/sweetrpg_model_core/__init__.py
@@ -10,5 +10,5 @@ __version__ = "0.0.96"
 __build__ = 0x00005e
 __author_email__ = "dm@sweetrpg.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2021 SweetRPG"
+__copyright__ = "Copyright 2023 SweetRPG"
 __cake__ = u"\u2728 \U0001f370 \u2728"
