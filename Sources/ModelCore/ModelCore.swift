e66fc6fd76f650086027f441b3d5fbddcde29ec0
Paul Schifferer <paul@schifferers.net>
2024-10-09 20:15:46 +0000

    Add Swift bits

---
+++ Sources/ModelCore/ModelCore.swift
@@ -0,0 +1,6 @@
+public struct ModelCore {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ tests/ModelCoreTests/ModelCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ModelCore
+
+final class ModelCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ModelCore().text, "Hello, World!")
+    }
+}
