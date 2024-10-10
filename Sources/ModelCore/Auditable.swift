f211d467fcca4312ce6687eefbb4296ce8257218
Paul Schifferer <paul@schifferers.net>
2024-10-10 14:21:56 +0000

    Default protocol implementation and tests

--- Sources/ModelCore/Auditable.swift
+++ Sources/ModelCore/Auditable.swift
@@ -8,3 +8,12 @@ public protocol Auditable {
     var deletedAt : Date? { get set }
     var deletedBy : String? { get set }
 }
+
+extension Auditable {
+    var createdAt : Date
+    var createdBy : String
+    var updatedAt : Date
+    var updatedBy : String
+    var deletedAt : Date?
+    var deletedBy : String?
+}
--- Tests/ModelCoreTests/AuditableTests.swift
+++ Tests/ModelCoreTests/AuditableTests.swift
@@ -1,11 +1,26 @@
 import XCTest
+import Foundation
 @testable import ModelCore
 
 final class AuditiableTests: XCTestCase {
-    func testExample() throws {
+    struct TestAuditable : Auditable {}
+
+    func testAuditable() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         // XCTAssertEqual(ModelCore().text, "Hello, World!")
+        let now = Date()
+        let user = "tester"
+        var auditable = TestAuditable(createdAt: now, createdBy: user,
+                                      updatedAt: now, updatedBy: user)
+
+        XCTAssertNotNil(auditable)
+        XCTAssertEqual(auditable.createdAt, now)
+        XCTAssertEqual(auditable.createdBy, user)
+        XCTAssertEqual(auditable.updatedAt, now)
+        XCTAssertEqual(auditable.updatedBy, user)
+        XCTAssertNil(auditable.deletedAt)
+        XCTAssertNil(auditable.deletedBy)
     }
 }
