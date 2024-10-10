a947eacffccf935da3a1352fcff256ec17284b3f
Paul Schifferer <paul@schifferers.net>
2024-10-10 03:46:26 +0000

    Accessors

--- Sources/ModelCore/Auditable.swift
+++ Sources/ModelCore/Auditable.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 public protocol Auditable {
-    var createdAt : Date
-    var createdBy : String
-    var updatedAt : Date
-    var updatedBy : String
-    var deletedAt : Date?
-    var deletedBy : String?
+    var createdAt : Date { get set }
+    var createdBy : String { get set }
+    var updatedAt : Date { get set }
+    var updatedBy : String { get set }
+    var deletedAt : Date? { get set }
+    var deletedBy : String? { get set }
 }
