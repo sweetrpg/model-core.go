5fbfe3acb708ef8815d23cd084d7dfe8e47b7d27
Paul Schifferer <paul@schifferers.net>
2024-10-10 01:37:49 +0000

    Update code

--- tests/ModelCoreTests/AuditableTests.swift
+++ tests/ModelCoreTests/AuditableTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import ModelCore
+@testable import Auditable
 
-final class ModelCoreTests: XCTestCase {
+final class AuditiableTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ModelCore().text, "Hello, World!")
+        // XCTAssertEqual(ModelCore().text, "Hello, World!")
     }
 }
