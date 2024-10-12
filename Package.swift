acf419406661e9223042e62786d610182a531aeb
Paul Schifferer <paul@schifferers.net>
2024-10-12 15:33:04 +0000

    Update package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ModelCore",
+    platforms: [
+       .macOS(.v13),
+    ],
     products: [
         .library(
             name: "ModelCore",
@@ -16,9 +19,17 @@ let package = Package(
     targets: [
         .target(
             name: "ModelCore",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: swiftSettings),
         .testTarget(
             name: "ModelCoreTests",
-            dependencies: ["ModelCore"]),
-    ]
+            dependencies: ["ModelCore"],
+            swiftSettings: swiftSettings),
+    ],
+    swiftLanguageModes: [.v5]
 )
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
