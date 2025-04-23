83337a16bfa5103dbcd0d8aacc0b6ab941089ba1
Paul Schifferer <paul@schifferers.net>
2025-04-23 06:34:26 -0700

    Move utility functions to a different package

--- vo/property.go
+++ vo/property.go
@@ -1,24 +1,7 @@
 package vo
 
-import (
-	"github.com/sweetrpg/common.go/util"
-	"github.com/sweetrpg/model-core.go/models"
-)
-
 type PropertyVO struct {
 	Name  string `json:"name" jsonapi:"attr,name"`
 	Kind  string `json:"kind" jsonapi:"attr,kind"`
 	Value string `json:"value" jsonapi:"attr,value"`
-}
-
-func FromPropertyModels(models []models.Property) []PropertyVO {
-	return util.Map(models, FromPropertyModel)
-}
-
-func FromPropertyModel(model models.Property) *PropertyVO {
-	return &PropertyVO{
-		Name:  model.Name,
-		Kind:  model.Kind,
-		Value: model.Value,
-	}
 }
--- vo/tag.go
+++ vo/tag.go
@@ -1,22 +1,6 @@
 package vo
 
-import (
-	"github.com/sweetrpg/common.go/util"
-	"github.com/sweetrpg/model-core.go/models"
-)
-
 type TagVO struct {
 	Name  string `json:"name" jsonapi:"attr,name"`
 	Value string `json:"value" jsonapi:"attr,value"`
-}
-
-func FromTagModels(models []models.Tag) []TagVO {
-	return util.Map(models, FromTagModel)
-}
-
-func FromTagModel(model models.Tag) *TagVO {
-	return &TagVO{
-		Name:  model.Name,
-		Value: model.Value,
-	}
 }
